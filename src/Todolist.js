e4f86a817e82c3fc8d46f5c6e4f7df87f24514ca
Lycanthrope <Taszidizaz@iut-dhaka.edu>
2023-07-16 22:46:34 +0600

    added the ticking functionality

--- src/Todolist.js
+++ src/Todolist.js
@@ -1,55 +1,58 @@
     import React, { useState } from 'react';
     import './App.css';
 
-
-
-    const Task = ({ task, deleteTask }) => {
-    return (
-        <li>
-        {task}
-        <button className='button' onClick={deleteTask}>Delete</button>
-        </li>
-    );
-    };
-
-
     const TodoList = () => {
     const [tasks, setTasks] = useState([]);
     const [newTask, setNewTask] = useState('');
-
 
     const handleInputChange = (event) => {
         setNewTask(event.target.value);
     };
 
-
     const addTask = (event) => {
         event.preventDefault();
-        setTasks([...tasks, newTask]);
+        if (newTask.trim() !== '') {
+        setTasks([...tasks, { task: newTask, completed: false }]);
         setNewTask('');
-        
+        }
     };
-
 
     const deleteTask = (index) => {
         const updatedTasks = [...tasks];
-        updatedTasks.splice(index, 1); // FIlter Can be used to remove too
+        updatedTasks.splice(index, 1);
+        setTasks(updatedTasks);
+    };
+
+    const toggleComplete = (index) => {
+        const updatedTasks = [...tasks];
+        updatedTasks[index].completed = !updatedTasks[index].completed;
         setTasks(updatedTasks);
     };
 
     return (
-        <div>
+        <div className="todo-list-container">
         <form onSubmit={addTask}>
-            <input type="text" value={newTask} onChange={handleInputChange} />
-            <button className='button2' type="submit">Add a Task</button>
+            <input
+            type="text"
+            value={newTask}
+            onChange={handleInputChange}
+            placeholder="Enter a task"
+            />
+            <button type="submit">Add Task</button>
         </form>
         <ul>
             {tasks.map((task, index) => (
-            <Task
-                key={index}
-                task={task}
-                deleteTask={() => deleteTask(index)}
-            />
+            <li key={index} className={task.completed ? 'completed' : ''}>
+                <input
+                type="checkbox"
+                checked={task.completed}
+                onChange={() => toggleComplete(index)}
+                />
+                <span className="task-text">{task.task}</span>
+                <button className="delete-button" onClick={() => deleteTask(index)}>
+                Delete
+                </button>
+            </li>
             ))}
         </ul>
         </div>
